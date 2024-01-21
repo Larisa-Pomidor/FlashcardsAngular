063373022bc272773b40ce935d8590f11f67c86f
Larisa Pomidor <nikakinikakin@gmail.com>
2024-01-21 15:55:48 +0200

    Cards list added

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cards',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.css'
+})
+export class CardsComponent {
+  cards: any;
+
+  constructor(private http: HttpClient) {
+    this.getAllCards();
+  }
+
+  getAllCards() {
+    this.http.get("http://localhost:8080/api/v1/cards")
+    .subscribe((response) => {
+      this.cards = response;
+    })
+  }
+
+  deleteCard(id: number) {
+    this.http.delete("http://localhost:8080/api/v1/cards/" + id).subscribe(
+      () => {
+        console.log('DELETE request successful');
+        this.cards = this.cards.filter((element: any) => element.id !== id);
+      },
+      (error) => {
+        console.error('Error during DELETE request:', error);
+      }
+    )  
+  }
+}
